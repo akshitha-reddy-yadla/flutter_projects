ee05335d5cf3cae524f8c89e0f6933d2a0fd116d
akshitha.yadla <akshithareddy.yadla@gmail.com>
2025-07-11 11:25:37 +0530

    remote and local data source

---
+++ number_trivia_app/test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:number_trivia_app/core/platform/network_info.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository_impl.dart';
+
+import 'number_trivia_repository_impl_test.mocks.dart';
+
+@GenerateMocks([NumberTriviaRemoteDataSource])
+@GenerateMocks([NumberTriviaLocalDataSource])
+@GenerateMocks([NetworkInfo])
+void main() {
+  NumberTriviaRepositoryImpl repository;
+  late MockNumberTriviaRemoteDataSource mockRemoteDataSource;
+  late MockNumberTriviaLocalDataSource mockLocalDataSource;
+  late MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mockRemoteDataSource = MockNumberTriviaRemoteDataSource();
+    mockLocalDataSource = MockNumberTriviaLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    repository = NumberTriviaRepositoryImpl(
+      remoteDataSource = mockRemoteDataSource,
+      localDataSource = mockLocalDataSource,
+      networkInfo = mockNetworkInfo,
+    );
+  });
+}
